4037934b4fdca089f48b73a3d31557c6024b97a3
sukesh <ssukesh54@gmail.com>
2021-01-29 12:13:46 +0530

    all

---
+++ ranjith/src/main/java/praneeth/shiva/mahesh/App.java
@@ -0,0 +1,13 @@
+package praneeth.shiva.mahesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
